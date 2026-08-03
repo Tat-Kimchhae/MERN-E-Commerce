6bbee6c04b533179c22100120049a0e9cedcd768
Kimchhae PC <your.email@example.com>
2026-08-03 15:39:00 +0700

    Update UI

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,12 +5,11 @@ import { ShopContext } from '../context/ShopContext';
 
 const NavBar = () => {
     const [visible, setVisible] = useState(false);
-    const {showSearch, setShowSearch, getCartCount} = useContext(ShopContext);
-    
+    const { showSearch, setShowSearch, getCartCount } = useContext(ShopContext);
+
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
             <Link to='/'> <img src={assets.logo} className='w-36' /> </Link>
-
 
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
                 <NavLink className='flex flex-col items-center gap-1' to='/'>
@@ -38,8 +37,9 @@ const NavBar = () => {
                 <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' />
 
                 <div className='group relative'>
-                    <img src={assets.profile_icon} className='w-5 cursor-pointer' />
-
+                    <Link to={'/login'}>
+                        <img src={assets.profile_icon} className='w-5 cursor-pointer' />
+                    </Link>
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                         <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                             <p className='cursor-pointer hover:text-black'> My Profile </p>
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsLetterBox from "../components/NewsLetterBox.jsx";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
+	return (
+		<div>
+			<div className='text-2xl text-center pt-8 border-t'>
+				<Title text1={'ABOUT'} text2={'US'} />
+			</div>
+			<div className="my-10 flex flex-col md:flex-row gap-16">
+				<img className='w-full md:max-w-112.5' src={assets.about_img} />
+				<div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+					<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur assumenda reprehenderit doloremque cumque? Facilis id quae dolor velit deserunt, eius consequatur corporis vitae ad maxime! Sed iste quod distinctio dolor. </p>
+					<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut condimentum enim. Quisque in porta augue, blandit suscipit purus. Donec felis elit, mollis cursus enim in, pellentesque maximus massa. Nunc facilisis sodales lorem, vel mattis augue sagittis ut. Proin eu metus sit amet diam dignissim congue. Donec placerat nulla varius, feugiat sapien id, iaculis dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin aliquam placerat elit sed dictum. Sed iste quod distinctio dolor. </p>
+					<b className='text-gray-800'> Our Mission </b>
+					<p> Pellentesque rutrum ex vitae dui ornare, at pharetra urna porttitor. Suspendisse elit nibh, tincidunt non massa et, tempus vulputate lacus. Praesent aliquet leo et justo aliquam, non condimentum enim pulvinar. Nunc at sem nunc. Aenean vehicula vel eros quis mattis. Aliquam erat volutpat. Praesent pretium risus sed lobortis imperdiet. </p>
+				</div>
+			</div>
+			<div className='text-4xl py-4'>
+				<Title text1={'WHY'} text2={'CHOOSE US'} />
+			</div>
+			<div className='flex flex-col md:flex-row text-sm mb-20'>
+				<div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+					<b> Quality Assurance: </b>
+					<p className={'text-gray-600'}> Tempus vulputate lacus. Praesent aliquet leo et justo aliquam, non condimentum enim pulvinar. Nunc at sem nunc. Aenean vehicula </p>
+				</div>
+				<div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+					<b> Convenience: </b>
+					<p className={'text-gray-600'}> Tempus vulputate lacus. Praesent aliquet leo et justo aliquam, non condimentum enim pulvinar. Nunc at sem nunc. Aenean vehicula </p>
+				</div>
+				<div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+					<b> Exceptional Customer Service: </b>
+					<p className={'text-gray-600'}> Tempus vulputate lacus. Praesent aliquet leo et justo aliquam, non condimentum enim pulvinar. Nunc at sem nunc. Aenean vehicula </p>
+				</div>
+			</div>
+			<NewsLetterBox/>
+		</div>
+	)
 }
 
 export default About
