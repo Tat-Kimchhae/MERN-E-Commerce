b2b3e5deea0171af72638aa155fbbfed8da7106b
Kimchhae <kimchhaedeveloper@gmail.com>
2026-08-22 20:01:21 +0700

    Update endpoint

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getOrders, getOrdersByUser, placeOrder, placeOrderByRazor, placeOrderByStripe, updateOrder} from "../controllers/ordersController.js";
+import {getOrders, getOrdersByUser, placeOrder, updateOrder} from "../controllers/ordersController.js";
 import {protect} from "../middleware/authMiddleware.js";
 import verifyAdmin from "../middleware/adminMiddleware.js";
 
@@ -8,8 +8,6 @@ const orderRouter = express.Router();
 // User endpoints
 orderRouter.get("/user", protect, getOrdersByUser);
 orderRouter.post("/place", protect, placeOrder);
-orderRouter.post("/place/stripe", protect, placeOrderByStripe);
-orderRouter.post("/place/razor", protect, placeOrderByRazor);
 
 // Admin endpoints
 orderRouter.get("", verifyAdmin, getOrders);
